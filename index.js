b7338081f47df490e8f239c357df19339786c8fa
KnightTaa <74761059+KnightTaa@users.noreply.github.com>
2021-09-27 15:33:23 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import 'dotenv/config'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import postRouter from './routes/post.js'
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', (err) => console.log(err))
+db.once('open', () => console.log("Database Connected"))
+
+const app = express()
+const PORT = process.env.PORT || 5000
+app.use(bodyParser.json())
+app.use('/posts', postRouter)
+
+app.listen(PORT, () => console.log(`Server started at port ${PORT}`))
